19306bb7ea20c133f938f9d8482ef5f41a42e848
dbernav <130405520+dbernav@users.noreply.github.com>
2024-11-21 15:02:17 -0500

    Entity class added, Player and Enemy will inherit

--- Chasm-CardGame/Assets/Scripts/gameManager.cs
+++ Chasm-CardGame/Assets/Scripts/gameManager.cs
@@ -6,10 +6,13 @@ using UnityEngine.UIElements;
 public class gameManager : MonoBehaviour
 {
 
+    public static gameManager instance;
 
-    [SerializeField] Transform _deck;
+    [SerializeField] Transform _deck; //internal deck - used to load and shuffle deck
 
-    private List<GameObject> mDeck;
+    private List<GameObject> mDeck; //main deck
+
+    private int pScore, eScore;
     
 
     // Start is called before the first frame update
@@ -26,22 +29,46 @@ public class gameManager : MonoBehaviour
 
     void LoadCards()
     {
-        foreach (Transform child in _deck)
+        foreach (Transform child in _deck) //grabs children to populate deck array to shuffle
         {
             mDeck.Add(child.gameObject);
         }
     }
     
-    void Shuffle()
+    void Shuffle() //Fisher-Yates/Knuth Shuffling Algorithm 
     {
         int rand_index;
         GameObject temp;
-        for (int lastIndex = mDeck.Count; lastIndex > 0; --lastIndex)
+        for (int lastIndex = mDeck.Count - 1; lastIndex > 0; --lastIndex)
         {
-            rand_index = Random.Range(0, lastIndex + 1);
+            rand_index = Random.Range(0, lastIndex); 
             temp = mDeck[lastIndex];
             mDeck[lastIndex] = mDeck[rand_index];
             mDeck[rand_index] = temp;
         }
+    }
+
+    void Deal()
+    { }
+
+    public void updateGameGoal()
+    {
+        if (pScore == 20)
+        {
+            Debug.Log("You Win!");
+        }
+        else if (eScore == 20)
+        {
+            Debug.Log("You Lose!");
+        }
+    }
+    void updatePoints()
+    {
+        //
+    }
+
+    void Discard()
+    {
+
     }
 }
